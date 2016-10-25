49166ad69e91ccf7b5af6860293364f7138f93b3
gmoshkin <louielouie314@gmail.com>
2016-10-25 17:52:19 +0300

    Update mytheme

--- themes/mytheme.sh
+++ themes/mytheme.sh
@@ -51,7 +51,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		"battery 1 15" \
 		# "rainbarf 0 0" \
 		"xkb_layout 6 15" \
-		"sound_volume 2 15" \
+		# "sound_volume 2 15" \
 		"currency 5 15"
 		"date_day 10 15" \
 		"date 10 15 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
