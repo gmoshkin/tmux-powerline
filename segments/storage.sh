4fdf4bb94b83ab0a56fe50148ccdb4faed507c5e
gmoshkin <louielouie314@gmail.com>
2016-10-25 17:29:55 +0300

    Add pretty unicode symbols to storage segment

--- segments/storage.sh
+++ segments/storage.sh
@@ -4,6 +4,9 @@ TMUX_POWERLINE_SEG_STORAGE_BOOT_PATTERN_DEFAULT="boot"
 TMUX_POWERLINE_SEG_STORAGE_ROOT_PATTERN_DEFAULT="ROOT"
 TMUX_POWERLINE_SEG_STORAGE_HOME_PATTERN_DEFAULT="home"
 TMUX_POWERLINE_SEG_STORAGE_DELIMITER_DEFAULT=""
+TMUX_POWERLINE_SEG_STORAGE_BOOT_SYMBOL_DEFAULT="👢"
+TMUX_POWERLINE_SEG_STORAGE_ROOT_SYMBOL_DEFAULT="√"
+TMUX_POWERLINE_SEG_STORAGE_HOME_SYMBOL_DEFAULT="🏠"
 DEFAULT_ROOT_PATTERN="\$6 == \"/\""
 
 generate_segmentrc() {
@@ -19,6 +22,12 @@ export TMUX_POWERLINE_SEG_STORAGE_HOME_PATTERN="${TMUX_POWERLINE_SEG_STORAGE_HOM
 # Can be "NONE" if not needed.
 # Delimiter character
 export TMUX_POWERLINE_SEG_STORAGE_DELIMITER="${TMUX_POWERLINE_SEG_STORAGE_DELIMITER_DEFAULT}"
+# Boot symbol
+export TMUX_POWERLINE_SEG_STORAGE_BOOT_SYMBOL="${TMUX_POWERLINE_SEG_STORAGE_BOOT_SYMBOL_DEFAULT}"
+# Root symbol
+export TMUX_POWERLINE_SEG_STORAGE_ROOT_SYMBOL="${TMUX_POWERLINE_SEG_STORAGE_ROOT_SYMBOL_DEFAULT}"
+# Home symbol
+export TMUX_POWERLINE_SEG_STORAGE_HOME_SYMBOL="${TMUX_POWERLINE_SEG_STORAGE_HOME_SYMBOL_DEFAULT}"
 
 EORC
     echo "$rccontents"
@@ -31,25 +40,29 @@ get_data() {
     /$TMUX_POWERLINE_SEG_STORAGE_HOME_PATTERN/ { home = \$4 \"/\" \$2 }
     END {
         res = \"\"
-        res = res boot
+        if (boot) {
+            res = res \"$TMUX_POWERLINE_SEG_STORAGE_BOOT_SYMBOL \"
+            res = res boot
+        }
         if (root) {
             if (res) {
-                res = res \" $TMUX_POWERLINE_SEG_STORAGE_DELIMITER \" root
-            } else {
-                res = root
+                res = res \" $TMUX_POWERLINE_SEG_STORAGE_DELIMITER \"
             }
+            res = res \"$TMUX_POWERLINE_SEG_STORAGE_ROOT_SYMBOL \"
+            res = res root
         }
         if (home) {
             if (res) {
-                res = res \" $TMUX_POWERLINE_SEG_STORAGE_DELIMITER \" home
-            } else {
-                res = home
+                res = res \" $TMUX_POWERLINE_SEG_STORAGE_DELIMITER \"
             }
+            res = res \"$TMUX_POWERLINE_SEG_STORAGE_HOME_SYMBOL \"
+            res = res home
         }
         print res
         # print boot, \"$TMUX_POWERLINE_SEG_STORAGE_DELIMITER\", root, \"$TMUX_POWERLINE_SEG_STORAGE_DELIMITER\", home
     }
     "
+    echo $TMUX_POWERLINE_SEG_STORAGE_HOME_SYMBOL
 }
 
 run_segment() {
