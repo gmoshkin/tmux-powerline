2814a82568a61b8c1faa301ca3034f669b26597c
Georgy Moshkin <louielouie314@gmail.com>
2017-04-12 22:56:09 +0300

    Update cmus procedure of now_playing segment
    Removed 4 invocations of a single program

--- segments/now_playing.sh
+++ segments/now_playing.sh
@@ -44,6 +44,10 @@ EORC
 	echo "$rccontents"
 }
 
+__log() {
+	echo "$@" > "${TMUX_POWERLINE_DIR_TEMPORARY}/now_playing.txt"
+}
+
 run_segment() {
 	__process_settings
 
@@ -182,21 +186,32 @@ __np_banshee() {
 }
 
 __np_cmus() {
-	#cmus-remote returns EXIT_FAILURE/EXIT_SUCCESS depending on whether or not cmus is running.
-	if cmus-remote -Q > /dev/null 2>&1; then
-		status=$(cmus-remote -Q | grep "status" | cut -d ' ' -f 2)
-		artist=$(cmus-remote -Q | grep -m 1 "artist" | cut -d ' ' -f 3-)
-		title=$(cmus-remote -Q | grep "title" | cut -d ' ' -f 3-)
-		#The lines below works fine. Just uncomment them and add them
-		# in np below if you want the track number or album name.
-		#tracknumber=$(cmus-remote -Q | grep "tracknumber" | cut -d ' ' -f 3)
-		#album=$(cmus-remote -Q | grep "album" | cut -d ' ' -f 3-)
-
-		np=$(echo "${artist} - ${title}")
-
-		if [ "$status" == "playing" ]; then
-			echo "$np"
-		fi
+	res=$(cmus-remote -Q |
+		awk -e '
+			/artist/ {
+				artist = "";
+				for ( i = 3; i <=NF; i++ )
+					artist = artist $i " ";
+			}
+			/title/ {
+				title = "";
+				for ( i = 3; i <=NF; i++ )
+					title = title $i " ";
+			}
+			/status/ {
+				status = $2
+			}
+			END {
+				print status "@" artist "- " title
+			}
+		'
+	)
+	np=${res/#*@/}
+	status=${res/%@*/}
+	if [[ "$status" == *playing* ]]; then
+		echo "$np"
+	else
+		__log "'$status@$np'"
 	fi
 }
 
