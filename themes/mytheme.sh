c6680f79fbff7a1ca1cc7c04edef29764d14e736
Georgy Moshkin <louielouie314@gmail.com>
2017-04-12 22:56:35 +0300

    Change now playing segments' colors

--- themes/mytheme.sh
+++ themes/mytheme.sh
@@ -47,9 +47,9 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"earthquake 3 0" \
 		"mailcount 1 15 90" \
 		# "pwd 12 15" \
-		"now_playing 8 6" \
 		# "cpu 240 136" \
 		"storage 12 15" \
+		"now_playing 13 15" \
 		"load 8 15" \
 		# "tmux_mem_cpu_load 234 136" \
 		"weather 4 15" \
