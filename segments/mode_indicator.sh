3c3aedf00b6f2c8cfd981f41a4746e384f9b20b0
gmoshkin <louielouie314@gmail.com>
2017-11-24 18:08:39 +0300

    Add support for selection_present format

--- segments/mode_indicator.sh
+++ segments/mode_indicator.sh
@@ -2,18 +2,20 @@
 
 PREFIX="☹"
 INSERT=" "
-COPY=""
+SELECT="★"
+COPY="★"
 NORM=" "
 SEP=" "
 
 PREFIX_FG="colour4"
 NORM_FG="colour0"
-COPY_FG="colour5"
+COPY_FG="colour2"
+SELECT_FG="colour5"
 bg="colour0"
 
 run_segment() {
     prefix_indicator="#{?client_prefix,#[fg=${PREFIX_FG}]${PREFIX},#[fg=${NORM_FG}]${NORM}}"
-    mode_indicator="#{?pane_in_mode,#[fg=${COPY_FG}]${COPY},${INSERT}}"
+    mode_indicator="#{?pane_in_mode,#{?selection_present,#[fg=${SELECT_FG}]${SELECT},#[fg=${COPY_FG}]${COPY}},${INSERT}}"
     echo $prefix_indicator"#[fg=${NORM_FG}]${SEP}"$mode_indicator""
     return 0
 }
