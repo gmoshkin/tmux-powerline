fdd49912941fd4d5ccbf72912b85f6f58c9b318c
Georgy Moshkin <louielouie314@gmail.com>
2017-12-07 12:10:40 +0300

    Minor refactoring

--- segments/mode_indicator.sh
+++ segments/mode_indicator.sh
@@ -7,15 +7,23 @@ COPY="★"
 NORM=" "
 SEP=" "
 
+bg="colour0"
 PREFIX_FG="colour4"
-NORM_FG="colour0"
+NORM_FG="$bg"
 COPY_FG="colour2"
 SELECT_FG="colour5"
-bg="colour0"
+INSERT_FG="$bg"
+
+PREFIX_FORMAT="#[fg=${PREFIX_FG}]${PREFIX}"
+NORM_FORMAT="#[fg=${NORM_FG}]${NORM}"
+SELECT_FORMAT="#[fg=${SELECT_FG}]${SELECT}"
+COPY_FORMAT="#[fg=${COPY_FG}]${COPY}"
+SEL_COP_FORMAT="#{?selection_present,$SELECT_FORMAT,$COPY_FORMAT}"
+INSERT_FORMAT="#[fg=${INSERT_FG}]${INSERT}"
 
 run_segment() {
-    prefix_indicator="#{?client_prefix,#[fg=${PREFIX_FG}]${PREFIX},#[fg=${NORM_FG}]${NORM}}"
-    mode_indicator="#{?pane_in_mode,#{?selection_present,#[fg=${SELECT_FG}]${SELECT},#[fg=${COPY_FG}]${COPY}},${INSERT}}"
+    prefix_indicator="#{?client_prefix,$PREFIX_FORMAT,$NORM_FORMAT}"
+    mode_indicator="#{?pane_in_mode,$SEL_COP_FORMAT,$INSERT_FORMAT}"
     echo $prefix_indicator"#[fg=${NORM_FG}]${SEP}"$mode_indicator""
     return 0
 }
